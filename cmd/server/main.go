513e8bffc0690470049715efeedeb7516a052e50
hubert le <namlem4u@gmail.com>
2025-03-30 04:26:01 +0700

    update set mode

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"product_srv/internal/database"
 	"product_srv/internal/routers"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+
 	// Load biến môi trường từ .env
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -17,6 +20,12 @@ func main() {
 		log.Fatal("Error loading .env file", err)
 		return
 	}
+	if os.Getenv("GO_ENV") == "development" {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	gin.SetMode(gin.ReleaseMode)
 
 	// 👉 Gán DB vào biến Global để sử dụng ở mọi nơi
 	database.NewDatabase()
